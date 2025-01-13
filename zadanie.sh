46b74fedcd20e68c198771ac832f70054231b33f
Aleksy Greiveldinger <s32987@pjwstk.edu.pl>
2025-01-13 13:28:10 -0500

    dodac do skryptu tworzenie dwoch plikow tekstowych i wypelnianie ich danymi w zaleznosci od 1. parametru.

--- zadanie.sh
+++ zadanie.sh
@@ -8,4 +8,26 @@ else
 	mkdir Kartki\ Swiateczne/{Kotki,Pieski}
 	mkdir Pozostale/{Listy\ zakupow,Przepisy}
 	mkdir Pozostale/Listy\ zakupow/{Swieta\ 2022,Swieta\ 2023}
+	
+	touch Pozostale/Przepisy/{Lista\ zakupow\ 2022.txt,Lista\ zakupow\ 2023.txt}
+	
+	if [ $1 == 2022 ] || [ $1 == 2023 ]
+	then
+		echo "świeża mięta
+1 kg jabłek
+1 kg cytryn
+500 g kiszonej kapusty
+1 mała główka świeżej kapusty
+80 g suszonych, leśnych grzybów
+200 g suszonych jabłek, śliwek, gruszek
+4 kg ziemniaków
+500 g drobnej, białej fasoli
+bułka tarta
+1 kg cukru
+1 l barszczu w kartonie" > Pozostale/Przepisy/Lista\ zakupow\ $1.txt
+	else
+		echo "Pierwszy parametr powinien miec wartosc 2022 lub 2023."
+	fi
+		
 fi
+
